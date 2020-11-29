a05318ba6f074d4ad16d9f63fb3f4cb0dca4f542
Brandon Krigbaum <brandonbk@gmail.com>
2020-11-29 08:48:46 -0600

    Add aw brand

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -1,4 +1,9 @@
 module.exports = {
+  aw: {
+    brandName: 'Automation World',
+    logoSrc: '/files/base/pmmi/aw/aw_logo.png',
+    subscribeLink: 'https://pmmi.dragonforms.com/loading.do?pk=AWMagNav&oly_enc_id=@{encrypted_customer_id}@&omedasite=PAWnew',
+  },
   pw: {
     brandName: 'Packaging World',
     logoSrc: '/files/base/pmmi/pw/pw_logo.png',
