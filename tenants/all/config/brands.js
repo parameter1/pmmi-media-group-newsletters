4c70e65ff876976eb058b6f871d1cd90c497591f
Brandon Krigbaum <brandonbk@gmail.com>
2020-12-06 17:00:12 -0600

    Add pfw brand

--- tenants/all/config/brands.js
+++ tenants/all/config/brands.js
@@ -4,6 +4,11 @@ module.exports = {
     logoSrc: '/files/base/pmmi/aw/aw_logo.png',
     subscribeLink: 'https://pmmi.dragonforms.com/loading.do?pk=AWMagNav&oly_enc_id=@{encrypted_customer_id}@&omedasite=PAWnew',
   },
+  pfw: {
+    brandName: 'Pro Food World',
+    logoSrc: '/files/base/pmmi/pfw/pfw_logo.png',
+    subscribeLink: 'https://pmmi.dragonforms.com/loading.do?r=&pk=PFW_BANNER_NL&utm_source=Newsletters&utm_medium=PFW%20Joyces%20Voice%20NL&utm_term=20200818&oly_enc_id=&omedasite=PPFWrenew',
+  },
   pw: {
     brandName: 'Packaging World',
     logoSrc: '/files/base/pmmi/pw/pw_logo.png',
