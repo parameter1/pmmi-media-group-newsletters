72d379cce62636db102d684cca426b15413b7742
Brandon Krigbaum <brandonbk@gmail.com>
2021-02-04 20:04:00 -0600

    Update email-x config

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -3,12 +3,12 @@ const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-emai
 const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://pmmi.serve.email-x.parameter1.com');
 
 config
-  .setAdUnits('replace-me', [
+  .setAdUnits('pw-editors-picks', [
     {
-      name: 'ad-slot-1',
-      id: 'replace-me',
-      width: 300,
-      height: 250,
+      name: 'slot-1',
+      id: '600dacd9b1fcca1d311ba4e5',
+      width: 600,
+      height: 100,
     },
   ]);
 
