d21c8357b8147437c1183fc2c774c9a90f1eefbd
Brandon Krigbaum <brandonbk@gmail.com>
2021-02-04 20:01:12 -0600

    Add newsletter configs

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -1,26 +1,104 @@
 const brands = require('./brands');
 const emailX = require('./email-x');
+const nativeX = require('./native-x');
 
 const config = {
   emailX,
+  nativeX,
   dpm: {
     emailx: {
       enabled: true,
     },
+  },
+
+  'aw-automation-skills': {
+    name: 'Automation Skills',
+    description: 'Automation applications and trends',
+    ...brands.aw,
+  },
+  'aw-continuous-processing': {
+    name: 'Continuous Processing',
+    description: 'News for industries where production flows without interruption',
+    ...brands.aw,
+  },
+  'aw-factory-automation': {
+    name: 'Factory Automation',
+    description: 'News from the discrete manufacturing industriesaw-',
+    ...brands.aw,
+  },
+  'aw-food-bev-pharma': {
+    name: 'Food, Bev & Pharma',
+    description: 'News from the consumer packaged goods industries',
+    ...brands.aw,
+  },
+  'aw-key-insights': {
+    name: 'Key Insights',
+    description: 'Insights for management, engineering, IT and operations',
+    ...brands.aw,
   },
   'aw-news-insights': {
     name: 'Automation News Insights',
     description: 'What\'s trending in the world of automation technology',
     ...brands.aw,
   },
+  'aw-product-insights': {
+    name: 'Automation Product Insights',
+    description: 'The latest innovations in automation products',
+    ...brands.aw,
+  },
+  'hcp-newsbrief': {
+    name: 'Newsbrief',
+    description: 'An editor-curated selection of life sciences packaging news and trends',
+    ...brands.hcp,
+  },
+  'oem-shop-talk': {
+    name: 'Shop Talk',
+    description: 'New trends and conversations for packaging and processing machine builders',
+    ...brands.pfw,
+  },
   'pfw-joyces-voice': {
-    name: 'Editors\' Picks',
+    name: 'Joyce\'s Voice',
     description: 'Innovation and Technology for Global Food & Beverage Processing',
     ...brands.pfw,
+  },
+  'pw-contract-packaging': {
+    name: 'Contract Packaging',
+    description: 'News, trends and strategies in contract packaging',
+    ...brands.pw,
   },
   'pw-editors-picks': {
     name: 'Editors\' Picks',
     description: 'Timely hand-picked favorites from <em>Packaging World</em>',
+    ...brands.pw,
+  },
+  'pw-end-of-line-strategies': {
+    name: 'End of Line Strategies',
+    description: 'Life at the intersection of packaging and logistics',
+    ...brands.pw,
+  },
+  'pw-packaging-insights': {
+    name: 'Contract Packaging',
+    description: 'Tips, Ideas + Best Practices',
+    ...brands.pw,
+  },
+  'pw-shelf-impact': {
+    name: 'Shelf Impact',
+    description: 'The brand manager\'s source for standing out in a crowd',
+    ...brands.pw,
+  },
+  'pw-machine-automation-insights': {
+    name: 'Machine Automation Insights',
+    description: 'Automation trends affecting packaging machinery',
+    ...brands.pw,
+  },
+  'pw-new-issue-alert': {
+    name: 'Machine Automation Insights',
+    description: 'Hot off the presses',
+    ...brands.pw,
+  },
+  'pw-new-machinery': {
+    name: 'Machine Automation Insights',
+    description: 'Get a glimpse at next gen equipment',
     ...brands.pw,
   },
 };
