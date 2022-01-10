fd178af45dd0f9eaf61858d234a42f84971905ec
Jessica Wade <jessica.wade@parameter1.com>
2022-01-10 13:58:38 -0600

    Create i18n.js file to config folder

---
+++ tenants/mundo/config/i18n.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'read more': 'Lea Más',
+  podcast: 'Escucha',
+  video: 'Reloj',
+  document: 'Descargar',
+  whitepaper: 'Descargar',
+  promotion: 'Aprende Más',
+  "what's trending": '¿Qué es tendencia?',
+  'articles, events and sponsored content to consider': 'Artículos, eventos y contenido patrocinado a considerar',
+  'presented by': 'Presentado por',
+};
