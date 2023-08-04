ffe3fe2dbe4d01da47c37ae91e240147c7c25d8e
Brian Miller <brian@parameter1.com>
2023-08-04 09:34:52 -0500

    lint:fix auto fixes

--- tenants/mundo/index.js
+++ tenants/mundo/index.js
@@ -14,7 +14,7 @@ module.exports = startServer({
   publicPath: 'public',
   onStart: (app) => {
     app.set('trust proxy', 'loopback, linklocal, uniquelocal');
-    set(app.locals, 'i18n', v => i18n[`${v}`.toLowerCase()] || v);
+    set(app.locals, 'i18n', (v) => i18n[`${v}`.toLowerCase()] || v);
   },
-  onAsyncBlockError: e => newrelic.noticeError(e),
-}).then(() => log('Newsletters started!')).catch(e => setImmediate(() => { throw e; }));
+  onAsyncBlockError: (e) => newrelic.noticeError(e),
+}).then(() => log('Newsletters started!')).catch((e) => setImmediate(() => { throw e; }));
