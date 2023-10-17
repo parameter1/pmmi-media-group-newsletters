731dfc6d3d7e93cf30a73ef6c69c6403986a1a1d
Jessica Wade <jessica.wade@parameter1.com>
2023-10-17 12:39:58 -0500

    Update mundo brand logo

--- tenants/mundo/config/brands.js
+++ tenants/mundo/config/brands.js
@@ -1,7 +1,7 @@
 module.exports = {
   mundo: {
     brandName: 'Mundo PMMI',
-    logoSrc: '/files/base/pmmi/mundo/mundo_logo.png',
+    logoSrc: '/files/base/pmmi/mundo/Mundo-EXPO-PACK_WEB.png',
     subscribeLink: 'https://pmmi.dragonforms.com/loading.do?pk=AWMagNav&oly_enc_id=@{encrypted_customer_id}@&omedasite=PAWnew',
   },
 };
