04c8f2a96a9e9ddc8c785ba5e106cdea88636e08
Jessica Wade <jessica.wade@parameter1.com>
2023-11-13 09:31:58 -0600

    Change NativeX placement name to ‘Subscription’

--- tenants/all/config/native-x.js
+++ tenants/all/config/native-x.js
@@ -243,7 +243,7 @@ module.exports = {
     },
     'pw-weekly': {
       download: '6539583845fe7b000179674e',
-      'pw-weekly': '654d31cfe9037c000173cfec',
+      subscription: '654d31cfe9037c000173cfec',
       promotion: '654d359bf2eda200016987f1',
     },
   },
