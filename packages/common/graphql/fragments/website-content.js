2617d24a5ddc071a42837efb29f349a992e55571
Jessica Wade <jessica.wade@parameter1.com>
2023-11-13 08:52:20 -0600

    Add website-content to graphql/fragments

---
+++ packages/common/graphql/fragments/website-content.js
@@ -0,0 +1,46 @@
+const gql = require('graphql-tag');
+
+module.exports = gql`
+
+fragment WebsiteContentListFragment on Content {
+  id
+  type
+  shortName
+  teaser(input: { useFallback: false, maxLength: null })
+  siteContext {
+    path
+  }
+  published
+  company {
+    id
+    type
+    name
+    siteContext {
+      path
+    }
+  }
+  primarySection {
+    id
+    name
+    fullName
+    canonicalPath
+  }
+  primaryImage {
+    id
+    src(input: { options: { auto: "format,compress" } })
+    alt
+    isLogo
+  }
+  ...on ContentEvent {
+    name(input: { mutation: Email })
+    ends
+    starts
+    startDate
+    endDate
+    city
+    state
+    website
+  }
+}
+
+`;
