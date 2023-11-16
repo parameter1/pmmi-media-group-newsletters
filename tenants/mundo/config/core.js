adc9b8946bfa038c9efce55d5ba7828c27f18bb2
Jessica Wade <jessica.wade@parameter1.com>
2023-11-16 14:29:41 -0600

    Add remaining brands to core.js files

--- tenants/mundo/config/core.js
+++ tenants/mundo/config/core.js
@@ -16,6 +16,21 @@ const config = {
     lang: 'es',
     ...brands.mundo,
   },
+  'mundo-expo-pack': {
+    brand: 'Mundo EXPO PACK',
+    title: 'OEM Daily',
+    lang: 'es',
+    headerSrc: '/files/base/pmmi/all/image/newsletters/mundo_expo_pack_revwhite.png',
+    primaryColor: '#007557',
+    socialMedia: {
+      imagePath: '/files/base/pmmi/all/image/static/newsletters/',
+      links: [
+        { provider: 'linkedin', href: 'https://www.linkedin.com/showcase/mundo-pmmi', target: '_blank' },
+        { provider: 'twitter', href: 'https://twitter.com/MundoPmmi', target: '_blank' },
+        { provider: 'facebook', href: 'https://www.facebook.com/MundoPMMI', target: '_blank' },
+      ],
+    },
+  },
 };
 
 module.exports = config;
