85584ade0086705cd365ac1a76903401e0aeb46f
Jessica Wade <jessica.wade@parameter1.com>
2024-01-03 08:02:18 -0600

    Delete old newsletters from config files

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -4,90 +4,10 @@ const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://
 
 config
 
-  .setAdUnits('hcp-serialization-countdown-new', [
-    {
-      name: 'bottom-audience-banner',
-      id: '625ed4056e4c9e529c3e7914',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('hcp-quick-hits-new', [
+  .setAdUnits('hcp-quick-hits', [
     {
       name: 'bottom-audience-banner',
       id: '6245fa366e4c9e5aff3e7525',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('oem-insights', [
-    {
-      name: 'bottom-audience-banner',
-      id: '60af9b0b72cf091c5e281ff6',
-      width: 600,
-      height: 100,
-    },
-  ])
-  .setAdUnits('pw-contract-packaging', [
-    {
-      name: 'bottom-audience-banner',
-      id: '6092992d932e2139a08030d8',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-editors-picks', [
-    {
-      name: 'bottom-audience-banner',
-      id: '600dacd9b1fcca1d311ba4e5',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-end-of-line-strategies', [
-    {
-      name: 'bottom-audience-banner',
-      id: '609299f5932e214266803110',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-packaging-insights', [
-    {
-      name: 'bottom-audience-banner',
-      id: '60929a70b1fccac9061ba996',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-shelf-impact', [
-    {
-      name: 'bottom-audience-banner',
-      id: '60929ac2932e2103298031b6',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-machine-automation-insights', [
-    {
-      name: 'bottom-audience-banner',
-      id: '60929b1c932e21866b80322e',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-new-issue-alert', [
-    {
-      name: 'bottom-audience-banner',
-      id: '60929b92b1fcca34a91baadf',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pw-new-machinery', [
-    {
-      name: 'bottom-audience-banner',
-      id: '60929bca932e2176288032e2',
       width: 560,
       height: 127,
     },
@@ -96,14 +16,6 @@ config
     {
       name: 'bottom-audience-banner',
       id: '622f3e3d394283ab9ef780e4',
-      width: 560,
-      height: 127,
-    },
-  ])
-  .setAdUnits('pfw-hand-picked', [
-    {
-      name: 'bottom-audience-banner',
-      id: '622f3e1d394283b2e8f780d1',
       width: 560,
       height: 127,
     },
