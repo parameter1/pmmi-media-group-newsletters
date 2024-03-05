88480962ca85571b2dd5abb8a67d1c01524bfc85
Jessica Wade <jessica.wade@parameter1.com>
2024-03-05 13:36:50 -0600

    Add 2nd promotion native ad placement for all daily

--- tenants/all/config/native-x.js
+++ tenants/all/config/native-x.js
@@ -26,26 +26,31 @@ module.exports = {
     },
     'pw-daily': {
       subscription: '65523d0df2eda200016e6cc8',
-      promotion: '6554c7a8e9037c00017b94ec',
+      'promotion-1': '6554c7a8e9037c00017b94ec',
+      'promotion-2': '65e72bb9f2eda200010892de',
     },
     'pfw-daily': {
       subscription: '65551789f2eda2000171e135',
-      promotion: '65551797e9037c00017c31ad',
+      'promotion-1': '65551797e9037c00017c31ad',
+      'promotion-2': '65e773d2f2eda20001090b53',
     },
     'oem-daily': {
       subscription: '65563bb8e9037c00017d42a2',
-      promotion: '65563bbef2eda2000172ed59',
+      'promotion-1': '65563bbef2eda2000172ed59',
+      'promotion-2': '65e773e9e9037c000113731e',
     },
     'hcp-daily': {
       subscription: '65563f28e9037c00017d4662',
-      promotion: '65563f32f2eda2000172f18e',
+      'promotion-1': '65563f32f2eda2000172f18e',
+      'promotion-2': '65e7740be9037c0001137384',
     },
     cpgnext: {
       'top-banner': '65157090f4a4a50001c2027c',
     },
     'cmp-daily': {
       subscription: '65776e85f2eda200019152ec',
-      promotion: '65776e90e9037c00019bb649',
+      'promotion-1': '65776e90e9037c00019bb649',
+      'promotion-2': '65e63473f2eda20001078df2',
     },
     'pw-new-issue': {
       subscription: '65d4dca2e9037c0001ffd0e3',
