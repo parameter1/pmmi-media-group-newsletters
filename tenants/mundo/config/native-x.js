3ac3a701f54c8ac665c6f53ed558ab5cdf4740a5
Jessica Wade <jessica.wade@parameter1.com>
2024-03-06 12:32:23 -0600

    Add promotion 2 native-x slot

--- tenants/mundo/config/native-x.js
+++ tenants/mundo/config/native-x.js
@@ -11,7 +11,8 @@ module.exports = {
     },
     'mundo-expo-pack': {
       subscription: '6556548fe9037c00017d6112',
-      promotion: '65565499e9037c00017d612c',
+      'promotion-1': '65565499e9037c00017d612c',
+      'promotion-2': '65e8abdae9037c0001153a45',
     },
   },
 };
