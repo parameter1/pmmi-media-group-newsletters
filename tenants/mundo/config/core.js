ba32e70f5feaaeadd43d92f48cf0fd4a65943321
Jessica Wade <jessica.wade@parameter1.com>
2025-10-29 13:50:20 -0500

    Update editor image, title, socials

--- tenants/mundo/config/core.js
+++ tenants/mundo/config/core.js
@@ -20,11 +20,10 @@ const config = {
       width: '640',
     },
     editor: {
-      src: '/files/base/pmmi/all/image/newsletters/mundo-editor.png',
+      src: '/files/base/pmmi/all/image/newsletters/editor-lilian.png ',
       width: '50',
       name: 'Lilián Robayo Páez',
-      title: 'Directora de Medios',
-      titleAddition: 'para Latinoamérica de PMMI',
+      title: 'Editora en jefe',
     },
     primaryColor: '#007557',
     socialMedia: {
@@ -32,7 +31,8 @@ const config = {
       links: [
         { provider: 'linkedin', href: 'https://www.linkedin.com/showcase/mundo-expopack/', target: '_blank' },
         { provider: 'youtube', href: 'https://www.youtube.com/channel/UCM5Ldhqi4SFRIyoGxuJZV3w', target: '_blank' },
-        { provider: 'facebook', href: 'https://www.facebook.com/MundoEXPOPACK', target: '_blank' },
+        { provider: 'applepodcast', href: 'https://podcasts.apple.com/mx/podcast/unpacked-with-pmmi-en-espa%C3%B1ol/id1748129616', target: '_blank' },
+        { provider: 'spotify', href: 'https://open.spotify.com/show/7Gkry7yJqDmsXFjqeBe68g?si=7034f76e6c554713&nd=1&dlsi=cdca54be9cb0432f', target: '_blank' },
       ],
     },
   },
